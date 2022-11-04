03f3d4edb00d2e5974c5609057e36425c2c2227c
snnbotchway <solomonbotchway7@gmail.com>
2022-11-04 19:06:29 +0000

    Add homepage

--- storefront/urls.py
+++ storefront/urls.py
@@ -23,6 +23,7 @@ admin.site.site_header = 'StoreFront Admin Panel'
 admin.site.index_title = 'Administration'
 
 urlpatterns = [
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
     path('playground/', include('playground.urls')),
     path('store/', include('store.urls')),
